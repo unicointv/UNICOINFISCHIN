041c0cc288a1c1fe13bb27b287c0c7e218e8cf07
unicointv <moneytreeirc@gmail.com>
2023-01-18 21:37:04 -0800

    it works

--- fischin.py
+++ fischin.py
@@ -3,7 +3,9 @@ import irc.strings
 import ssl
 import random
 import yaml
-from datetime import datetime
+import sched, time
+import threading
+from datetime import datetime, timedelta
 from tinydb import TinyDB, Query
 
 class FishingBot(irc.bot.SingleServerIRCBot):
@@ -15,16 +17,8 @@ class FishingBot(irc.bot.SingleServerIRCBot):
         self.User = Query()
         self.nickname = nickname
         self.players = {}
-        self.items = {}
-        self.events = {}
-        self.load_content()
-
-    def load_content(self):
-        with open("content.yaml", 'r') as stream:
-            data = yaml.safe_load(stream)
-            self.items = data['items']
-            self.events = data['events']
-            stream.close()
+        with open('content.yaml', 'r', encoding='utf-8') as yaml_file:
+            self.content = yaml.safe_load(yaml_file)
 
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
@@ -43,51 +37,81 @@ class FishingBot(irc.bot.SingleServerIRCBot):
             self.check_money(c, nick)
     
     def cast(self, c, nick):
-        self.players[nick] = {"status": "casting", "cast_time": datetime.now()}
-        self.send_response(c, nick, random.choice(self.events["cast"]["responses"]).format(n = nick))
-        c.execute_delayed(random.randint(5, 10), self.bite, (c, nick))
+            self.players[nick] = {"status": "casting", "cast_time": datetime.now()}
+            for event in self.content["events"]:
+                if event["name"] == "cast":
+                    self.send_response(c, random.choice(event["responses"]).format(n = nick))
+            timer_thread = threading.Thread(target=self.start_timer, args=(c, nick))
+            timer_thread.start()
+
+    def start_timer(self, c, nick):
+        time.sleep(random.randint(5,10))
+        self.bite(c, nick)
 
     def bite(self, c, nick):
-        self.players[nick]["status"] = "biting"
-        self.players[nick]["biting_time"] = datetime.now()
-        self.send_response(c, nick, random.choice(self.events["bite"]["responses"]).format(n = nick))
+        self.players[nick].update({"status": "biting", "bite_time": datetime.now()})
+        for event in self.content["events"]:
+            if event["name"] == "bite":
+                self.send_response(c, random.choice(event["responses"]).format(n = nick))
+
+
 
     def reel(self, c, nick):
         if nick in self.players:
             if self.players[nick]["status"] == "biting":
-                if self.players[nick]["biting_time"] - self.players[nick]["cast_time"] < 15:
-                        items = self.items
+                if (self.players[nick]["bite_time"] - self.players[nick]["cast_time"]) < timedelta(seconds=15):
+                        items = self.content["items"]
                         item = random.choices(items,weights=[1 for item in items],k=1)
                         if random.randint(0,100)< 50:
                             self.players[nick]["status"] = "idle"
-                            self.players[nick]["money"] += item[0]["value"]
-                            self.send_response(c, nick, random.choice(self.item[0]["responses"]).format(n = nick))
+                            user_data = self.db.search(self.User.nick == nick)
+                            if user_data:
+                                current_balance = user_data[0]["balance"]
+                                new_balance = current_balance + item[0]["value"]
+                                self.db.update({"balance": new_balance}, self.User.nick == nick)
+                            else:
+                                self.db.insert({"nick":nick, "balance": 0})
+                            self.send_response(c, random.choice(item[0]["responses"]).format(n = nick))
                         else:
                             self.players[nick]["status"] = "idle"
-                            self.send_response(c, nick, random.choice(self.events["fail_catch"]["responses"]))
+                            for event in self.content["events"]:
+                                if event["name"] == "fail_catch":
+                                    self.send_response(c, random.choice(event["responses"]).format(n = nick))
                 else:
                     self.players[nick]["status"] = "idle"
-                    self.send_response(c, nick, random.choice(self.events["fail_catch"]["responses"]).format(n = nick))
+                    for event in self.content["events"]:
+                        if event["name"] == "fail_catch":
+                            self.send_response(c, random.choice(event["responses"]).format(n = nick))
             else:
-                self.send_response(c, nick, random.choice(self.events["premature_reel"]["responses"]).format(n = nick))
+                for event in self.content["events"]:
+                    if event["name"] == "premature_reel":
+                        self.send_response(c, random.choice(event["responses"]).format(n = nick))
         else:
             pass
 
     def check_money(self, c, nick):
-        with self.db.tiny('money') as money_db:
-            user_data = money_db.search(self.User.nick == nick)
+            user_data = self.db.search(self.User.nick == nick)
             if user_data:
-                self.send_response(nick, random.choice(self.events["check_money"]["responses"]).format(money=user_data[0]["balance"], n=nick))
+                for event in self.content["events"]:
+                    if event["name"] == "check_money":
+                        self.send_response(c,  random.choice(event["responses"]).format(money=user_data[0]["balance"], n=nick))
             else:
-                self.send_response(nick, random.choice(self.events["check_money"]["responses"]).format(money=0, n=nick))
+                for event in self.content["events"]:
+                    if event["name"] == "check_money":
+                        self.db.insert({"nick":nick, "balance": 0})
+                        self.send_response(c, random.choice(event["responses"]).format(money=0, n=nick))
 
     def send_response(self, c, response):
+        color_pairs = [("01","07"),("03","08"),("02","10"),("06","09"),("11","07"),("03","05"),("08","12")]
+        color_pair = random.choice(color_pairs)
+        text_color, bg_color = color_pair
+        response = "\x03" + text_color + "," + bg_color + response
         c.privmsg(self.channel, response)
         
     def on_ping(self, c, e):
         c.pong(e.arguments[0])
     
-def main(self):
+def main():
         bot = FishingBot(channel="#gamme", nickname="unicoin", server="irc.libera.chat")
         bot.start()
 
